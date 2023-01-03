9ab44c434cfc1a9d11ae7543d78d9fdcef76cc2c
lingen <*\(^_^)/*>
2023-01-03 15:47:40 +0800

    重复方法

--- file/index.go
+++ file/index.go
@@ -5,25 +5,6 @@ import(
 	"path"
 	"strings"
 )
-/*
-递归遍历文件夹
-*/
-func ReadDirDeep(base string, list *[]string, dirList *[]string) {
-	fileList, _ := os.ReadDir(base)
-	for _, f := range fileList {
-		if f.IsDir() {
-			// 存下生成的目录
-			if dirList != nil {
-				*dirList = append(*dirList, path.Join(base, f.Name()))
-			}
-			ReadDirDeep(path.Join(base, f.Name()), list, dirList)
-		} else {
-			//  存下生成的文件
-			*list = append(*list, path.Join(base, f.Name()))
-		}
-	}
-}
-
 
 /*
 复制文件夹
