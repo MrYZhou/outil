69df38879575660b746b302c902fd4cd902db26c
lg <1762861715@qq.com>
2024-01-08 12:37:37 +0800

    优化控制台输出

--- command/index.go
+++ command/index.go
@@ -12,9 +12,16 @@ import (
 第一个参数是执行的目录,第二个参数是命令
 */
 func Run(content string, direct string) {
+	// 将direct按空格分割为参数列表
 	param := strings.Split(direct, " ")
+	// 创建一个执行命令的命令对象cmd，并设置要执行的命令和参数
 	cmd := exec.Command(param[0], param[1:]...)
+	// 设置要执行命令的当前工作目录为content
 	cmd.Dir = content
+	// 将命令的标准输出重定向到os.Stdout
 	cmd.Stdout = os.Stdout
-	cmd.Run()
+	// 将命令的错误输出重定向到os.Stderr
+	cmd.Stderr = os.Stderr
+	// 执行命令,实时输出
+	cmd.Start()
 }
